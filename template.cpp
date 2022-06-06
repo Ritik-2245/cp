0768c5a3eae4c7475333569b58042f1db134fd30
RITIK KUMAR <94675803+Ritik-2245@users.noreply.github.com>
2022-06-06 22:01:52 +0530

    Update template.cpp

---
+++ template.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define v(x) vector<x>
+#define lb lower_bound
+#define ub upper_bound
+#define u_m unordered_map
+#define fe first
+#define se second
+#define loop(i,l,r) for(int i=l;i<r;i++)
+#define bn begin
+#define en end
+#define int long long
+#define setz fixed<<setprecision(6)
+#define print(s) cout<<s<<endl
+int cint();
+double cdouble();
+char schar();
+string gstr();
+string cstr();
+
+
+void solve(){
+
+
+
+}
+
+
+
+
+
+
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout.tie(NULL);
+
+
+    int t=cint();
+    while (t--)
+    {
+     solve();      
+    }
+
+    return 0;
+
+
+}
+
+int cint(){
+	int k;
+	cin>>k;
+	return k;
+}
+
+double cdouble(){
+	double k;
+	cin>>k;
+	return k;
+}
+
+string gstr(){
+	string s;
+
+	getline(cin,s);
+	return s;
+}
+
+string cstr(){
+	string s;
+	cin>>s;
+	return s;
+}
+
+char schar(){
+	char c;
+	cin>>c;
+	return c;
+}
