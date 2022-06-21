47693a1f86638acc22a33925fe6971f61138bc83
RITIK KUMAR <94675803+Ritik-2245@users.noreply.github.com>
2022-06-21 08:52:01 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,86 +2,93 @@
 using namespace std;
 #define pb push_back
 #define v(x) vector<x>
+#define vvi(x) vector<vector<x>>
+#define vp(x, y) vector<pair<x, y>>
 #define lb lower_bound
 #define ub upper_bound
 #define u_m unordered_map
-#define fe first
+#define fi first
 #define se second
-#define loop(i,l,r) for(int i=l;i<r;i++)
+#define loop(i, l, r) for (int i = l; i < r; i++)
 #define bn begin
 #define en end
 #define int long long
-#define setz fixed<<setprecision(6)
-#define print(s) cout<<s<<'\n'
+#define setz(x) fixed << setprecision(x)
+#define print(s) cout << s << endl
+#define ps(s) cout << s << ' '
+const int MOD = 1e9 + 7;
 int cint();
+unsigned long long uint();
 double cdouble();
 char schar();
 string gstr();
 string cstr();
 
+void solve()
+{
 
-void solve(){
-
-
-
+ 
 }
-
-
-
-
 
 
 int32_t main()
 {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout.tie(NULL);
-
-
-    int t=cint();
-    while (t--)
+    int tc = cint();
+    while (tc--)
     {
-     solve();      
+        solve();
     }
 
     return 0;
-
-
 }
 
-int cint(){
-	int k;
-	cin>>k;
-	return k;
+int cint()
+{
+    int k;
+    cin >> k;
+    return k;
 }
 
-double cdouble(){
-	double k;
-	cin>>k;
-	return k;
+double cdouble()
+{
+    double k;
+    cin >> k;
+    return k;
 }
 
-string gstr(){
-	string s;
+string gstr()
+{
+    string s;
 
-	getline(cin,s);
-	return s;
+    getline(cin, s);
+    return s;
 }
 
-string cstr(){
-	string s;
-	cin>>s;
-	return s;
+string cstr()
+{
+    string s;
+    cin >> s;
+    return s;
 }
 
-char schar(){
-	char c;
-	cin>>c;
-	return c;
+char schar()
+{
+    char c;
+    cin >> c;
+    return c;
+}
+
+unsigned long long uint()
+{
+    unsigned long long k;
+    cin >> k;
+    return k;
 }
